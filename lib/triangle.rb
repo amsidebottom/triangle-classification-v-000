110c57c7235f14028395033306ec9f463798e47b
Alyssa Dulin <alyssa@alyssadulin.com>
2018-05-05 15:38:11 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,16 +9,16 @@ class Triangle
   
   def kind
     if @length1 <= 0 or @length2 <= 0 or @length3 <= 0 
-       begin
+      begin
         raise TriangleError
       rescue TriangleError => error
-        puts error.message
+        puts TriangleError.message
       end
     elsif (@length1 + @length2 < @length3) or (@length2 + @length3 < @length1) or (@length1 + @length3 < @length2)
       begin
         raise TriangleError
       rescue TriangleError => error
-        puts error.message
+        puts TriangleError.message
       end
     else
       if @length1==@length2 and @length2==@length3
