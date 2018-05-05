b2e8dc1d04c55241ccbc5d8d32fa6a4ff884d5e4
Alyssa Dulin <alyssa@alyssadulin.com>
2018-05-05 11:44:51 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,8 @@ class Triangle
   end
   
   def kind
-
+    if @length1 = 0 or @length2 = 0 or @length3 = 0 or @length1+ @length2 < @length3 or @length2 + @length3 < @length1 or @length1 + @length3 < @length2
+      
     if @length1=@length2 and @length2=@length3
       return equilateral
     elsif @length1=@length2 or @length2=@length3 or @length1=@length3
